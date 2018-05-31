664f067c3c6ef315090ee73fe230761f06113614
Karl Amrhein <karlamrhein@gmail.com>
2018-05-31 05:54:52 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void) 
+{
+
+ int pid;
+ pid = fork();
+ pid = fork();
+ if (pid == -1) 
+ {
+    perror("failed: ");
+    exit(123);
+ }
+ if (pid ==  0) 
+ {
+    printf("child == %d, %d\n", getpid(), pid);
+    sleep(90);
+    _exit(0);
+ }
+ printf("parent == %d, and my child == %d\n", getpid(), pid);
+ waitpid(pid, 0, 0);
+ exit(0);
+}
