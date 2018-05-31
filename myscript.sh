4de76a2d13b56cd4f627902730a5170d6e2ded7e
Karl Amrhein <karlamrhein@gmail.com>
2018-05-31 11:16:38 -0600

    update

--- myscript.sh
+++ myscript.sh
@@ -18,6 +18,7 @@ who
 curl -L -O http://openafs.org/dl/openafs/1.6.22.3/openafs-1.6.22.3-src.tar.gz
 tar zxvf openafs-1.6.22.3-src.tar.gz
 cd openafs-1.6.22.3
-./configure --enable-transarc-paths
+
+# ./configure --enable-transarc-paths
 # make
 # make dest
