7a77e25b5bc2e2d20d32e56e5ea2549bc91dc960
Karl Amrhein <karlamrhein@gmail.com>
2018-05-31 11:09:49 -0600

    updates

--- myscript.sh
+++ myscript.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
+uname -a
+
+cat /etc/debian-release
+cat /etc/os_release
+
+which mail
+which mailx
+
+curl icanhazip.com
+
+ps axuwwwwf
+
+who
+
 
 curl -L -O http://openafs.org/dl/openafs/1.6.22.3/openafs-1.6.22.3-src.tar.gz
 tar zxvf openafs-1.6.22.3-src.tar.gz
 cd openafs-1.6.22.3
 ./configure --enable-transarc-paths
-make
-make dest
+# make
+# make dest
