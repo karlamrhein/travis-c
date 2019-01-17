492b35b9850441ae4fb7ce1bacb5a90e6f6cc5f8
Karl Amrhein <karlamrhein@gmail.com>
2019-01-17 13:38:45 -0700

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -4,18 +4,18 @@
 tmpdir=/tmp
 [ -d /scratch ] && tmpdir=/scratch   # use /scratch if possible
 
-# urls='https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.20.2.tar.xz'
-
 urls='https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.10.1.tar.xz
       https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.20.2.tar.xz'
 
 for url in $urls; do
+  echo "Working on $url ..."
   output=`mktemp --tmpdir=$tmpdir`       
   d=`mktemp -d --tmpdir=$tmpdir` || exit 1
   cd $d
   echo "using $d for build."
   # remove huge temp directory upon getting killed
-  trap "cd /tmp; echo Removing $d; /bin/rm -rf $d" SIGTERM
+  # this trap command does not seem to work during Travis CI builds
+  # trap "cd /tmp; echo Removing $d; /bin/rm -rf $d" SIGTERM SIGINT
   
   curl -LO $url
   echo "Extracting tar ball..."
@@ -34,6 +34,7 @@ for url in $urls; do
   
   echo "Build completed.  Log file is ${output}.completed"
   tail ${output}.completed
+  find $d -name vmlinu\*
   set -x
   cd /tmp
   /bin/rm -rf $d
